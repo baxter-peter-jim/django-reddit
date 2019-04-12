1121b83cab33515bdc826ba16426975b8893ce99
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-04-12 14:54:01 -0400

    add Profile and Post_Vote models

--- django_reddit/reddit/models.py
+++ django_reddit/reddit/models.py
@@ -28,3 +28,41 @@ class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete='CASCADE', related_name='comments'
     )
+
+
+def __str__(self):
+    return self.content
+
+
+class Profile(models.Model):
+    profile_pic = models.CharField(max_length=400)
+    user = models.ForeignKey(
+        User, on_delete='CASCADE', related_name='profile'
+    )
+
+
+def __str__(self):
+    return self.profile_pic
+
+
+class Post_Vote(models.Model):
+    user = models.ForeignKey(
+        User, on_delete='CASCADE', related_name='post_votes'
+    )
+    post = models.ForeignKey(
+        Post, on_delete='CASCADE', related_name='post_votes'
+    )
+    plusOne = '+1'
+    minusOne = '-1'
+    POST_VOTE_CHOICES = (
+        (plusOne, '+1'),
+        (minusOne, '-1')
+    )
+    value = models.CharField(
+        max_length=2,
+        choices=POST_VOTE_CHOICES
+    )
+
+
+def __str__(self):
+    return self.value
