41b0fda75e7388c6106fafbee9ff6f631e719102
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-04-12 14:32:00 -0400

    added user model

--- django_reddit/reddit/models.py
+++ django_reddit/reddit/models.py
@@ -1,30 +1,11 @@
 from django.db import models
 
+# Create your models here.
 
-class Post(models.Model):
-    created_at = models.CharField(max_length=1000)
-    title = models.TextField(max_length=1000)
-    picture = models.TextField(max_length=1000)
-    content = models.TextField(max_length=1000)
-    site_url = models.TextField(max_length=1000)
-    vote_total = models.TextField(max_length=1000)
-    # user = models.ForeignKey(
-    #     User, on_delete='CASCADE', related_name='users')
+class User(models.Model):
+    email: models.CharField(max_length=100)
+    password: models.CharField(max_length=100)
+    username: models.CharField(max_length=100)
 
-
-def __str__(self):
-    return self.created_at
-
- # Comment Model
-
-
-class Comment(models.Model):
-    created_at = models.CharField(max_length=400)
-    content = models.CharField(max_length=400)
-    vote_total = models.CharField(max_length=400)
-    user = models.ForeignKey(
-        User, on_delete='CASCADE', related_name='comments'
-    )
-    post = models.ForeignKey(
-        Post, on_delete='CASCADE', related_name='comments'
-    )
+    def __str__(self):
+        return self.username
