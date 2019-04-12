c92d0568abda4918a3c379a75c05f73425b59348
Jim Doan <jimdoan4@gmail.com>
2019-04-12 15:46:28 -0400

    created forms.py and user/registration

--- django_reddit/reddit/admin.py
+++ django_reddit/reddit/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+from .models import Comment
+from .models import User
+from .models import Profile
+from .models import Save
+from .models import Post_Vote
+from .models import Comment_Vote
+from .models import Moderator
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Save)
+admin.site.register(Post_Vote)
+admin.site.register(Comment_Vote)
+admin.site.register(Moderator)
---
+++ django_reddit/reddit/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from .models import Post, Comment, User, Profile, Save, Post_Vote, Comment_Vote, Moderator
+
+
+class PostForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Post
+        fields = ('created_at', 'title', 'picture',
+                  'content', 'site_url', 'vote_total', 'user')
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Comment
+        fields = ('created_at', 'content', 'vote_total', 'user', 'post')
+
+
+class UserForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = User
+        fields = ('email', 'password', 'username')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Profile
+        fields = ('profile_pic', 'user')
+
+
+class SaveForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Save
+        fields = ('user', 'post', 'created_at')
+
+
+class Post_VoteForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Post_Vote
+        fields = ('user', 'post', 'value')
+
+
+class Comment_VoteForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Comment_Vote
+        fields = ('user', 'comment', 'value')
+
+
+class ModeratorForm(forms.ModelForm):
+    class Meta:  # This Meta class is something you will see every now and then in Python.  It usually contains config info.
+        model = Moderator
+        fields = ('user')
