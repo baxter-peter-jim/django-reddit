e5c7ed21dce336936b7fa7312ce4417bd3d07dd6
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-04-12 12:06:09 -0400

    add project folder and app

--- django_reddit/django_reddit/settings.py
+++ django_reddit/django_reddit/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_lljjc=(a0o73-&4-j7dd!ol(rdkhs0tlfm5da$-1smtf9u(^b'
+SECRET_KEY = '(2a^hhd-om5j+iae5tp8ccn9%8%05ahj80ksq(z_(zc+5qz!3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [],
+ALLOWED_HOSTS = []
 
 
 # Application definition
